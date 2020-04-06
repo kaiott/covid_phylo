008c6095f917e760d6709b9466a86690b363d4f1
Kai Ott <ottka@student.ethz.ch>
2020-04-06 19:51:03 +0200

    write to fasta format

--- src/config.py
+++ src/config.py
@@ -7,5 +7,7 @@ BASE_DIR = PROJECT_DIR / 'covid_phylo_data'
 BASE_DIR.mkdir(exist_ok=True)
 
 CACHE_DIR = BASE_DIR / 'cache'
-	
+
+FASTA_DIR = BASE_DIR / 'fasta'
+
 RAW_SEQUENCE_SHELVE_FNAME = 'raw_seqs.shelve'
--- src/main.py
+++ src/main.py
@@ -3,6 +3,30 @@ import ncbi
 import config
 
 
+def records_to_fasta(records, fasta_dir=None, filename='records.txt'):
+	if records is None:
+		return None
+	
+	fasta_result = {}
+	content = ''
+	for record in records:
+		fasta = record.format('fasta')
+		if fasta_dir is not None:
+			content += fasta
+		
+		[header_line, sequence] = fasta.split('\n', 1)
+		print(header_line)
+		fasta_result[header_line] = sequence
+
+	if fasta_dir is not None:
+		fasta_dir.mkdir(exist_ok=True)
+		f = open(fasta_dir / filename, 'w')
+		f.write(content)
+		f.close()
+
+	return fasta_result
+
+
 def main():
     """
     DESCRIPTION:
@@ -10,8 +34,8 @@ def main():
     :return: None. Execution of the instructions.
     """
     result = ncbi.get_all_covid_nucleotide_seqs(cache_dir=config.CACHE_DIR)
-    record = result.get('seqrecords')[0]
-    print(record)
+    records = result.get('seqrecords')
+    records_to_fasta(records, config.FASTA_DIR, 'IUPACAmbiguousDNA.txt')
 
 
 if __name__ == '__main__':
