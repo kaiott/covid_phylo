a568334d16b10f28fb852c4e4ae5b7ca28a7ef24
mrubio-chavarria <msrch@hotmail.es>
2020-05-02 09:07:55 +0200

    Add functions description Change os employment by subprocess employment Add pathlib route in image creation

--- src/ete.py
+++ src/ete.py
@@ -4,11 +4,16 @@ from config import MEDIA_DIR
 from ete3 import Tree, TreeStyle
 
 
-def newick_extractor(fileroute):
-    pass
-
 def tree_viewer(treefolder):
+    """
+    DESCRIPTION:
+    A function to create the representation of the tree with ete. Given the folder with the files in the tree folder it
+    creates the tree representation in the media folder.
+    :param treefolder: [pathlib] route to the subfolder in the tree folder which contains the data of the inference.
+    :return: None. It writes the image in the media folder.
+    """
     filename = os.path.basename(os.path.normpath(treefolder)) + '.txt.treefile'
+    imagefile = os.path.basename(os.path.normpath(treefolder)) + '.png'
     treeroute = treefolder / filename
     file = open(treeroute, 'r')
     tree = file.read()
@@ -17,4 +22,4 @@ def tree_viewer(treefolder):
     circular_style = TreeStyle()
     circular_style.mode = "c"
     circular_style.scale = 20
-    tree.render(str(MEDIA_DIR / 'mytree.png'), w=1024, units='mm', tree_style=circular_style)
+    tree.render(str(MEDIA_DIR / imagefile), w=1024, units='mm', tree_style=circular_style)
--- src/iqtree.py
+++ src/iqtree.py
@@ -1,17 +1,33 @@
 
 import os
+import subprocess
+
 from config import MEDIA_DIR, TREE_DIR, FASTA_DIR
 
 
 def tree_creator(selectname):
+    """
+    DESCRIPTION:
+    A function create the tree inference and store the results in a subfolder within covid_phylo/tree/
+    :param selectname: [string] name of the file to be put in the tree folder. The same as the name of the subfolder.
+    :return: None
+    """
     print('Executing tree inference')
     filename = selectname
     subfolder = selectname.split('.')[0]
-    os.system(f'iqtree -s {TREE_DIR / subfolder / filename}')
-    print('Tree inference completed')
+    process = subprocess.run(['iqtree', '-s', f'{TREE_DIR / subfolder / filename}', '-bnni', '-nt', 'AUTO'])
+    print('Tree inference completed with exit code %d' % process.returncode)
 
 
 def align_selector(origname, destname, n_genomes):
+    """
+    DESCRIPTION:
+    Function to select the n alignments with the lowest number of gaps.
+    :param origname: [string] name of the file with the complete list of alignments in the fasta folder.
+    :param destname: [string] name of the file to be put in the tree folder. The same as the name of the subfolder.
+    :param n_genomes: [integer] number of alignments to be taken.
+    :return: None. It writes the selected aignments in the destname folder.
+    """
     # Part to take the alignments with lowest number of gaps
     file = open(FASTA_DIR / origname, 'r')
 
