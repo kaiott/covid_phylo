801f71b714ad98db151d0915a4816f000177df9a
Kai Ott <kai.ott@bluewin.ch>
2020-05-05 02:58:29 +0200

    change main examples

--- src/align_tools.py
+++ src/align_tools.py
@@ -76,6 +76,10 @@ class SequenceAligner:
             print('no unaligned sequences')
             return
 
+        if len(sequence_ids_written) == 1:
+            print('only one sequence')
+            return
+
         print(f'will align {len(sequence_ids_written)} new sequences')
 
         if self.already_aligned_file_id is None:
@@ -86,7 +90,7 @@ class SequenceAligner:
             self._align_from_existing()
 
         if make_copy:
-            self._copy_aligned_file_unstamped()
+            self.copy_aligned_file_unstamped()
 
         # at this point assume alignment done successfully
         # update meta information
@@ -98,12 +102,15 @@ class SequenceAligner:
         with open(SequenceAligner.used_dir / info_filename, 'w') as file:
             file.write(json.dumps(info_dict))
 
-    def _copy_aligned_file_unstamped(self):
-        in_filename = SequenceAligner.used_dir / self.get_aligned_filename()
-        out_filename = SequenceAligner.used_dir / f'{self.tag}_aligned'
-        with open(in_filename, 'r') as in_file, open(out_filename, 'w') as out_file:
-            content = in_file.read()
-            out_file.write(content)
+    def copy_aligned_file_unstamped(self):
+        try:
+            in_filename = SequenceAligner.used_dir / self.get_aligned_filename()
+            out_filename = SequenceAligner.used_dir / f'{self.tag}_aligned'
+            with open(in_filename, 'r') as in_file, open(out_filename, 'w') as out_file:
+                content = in_file.read()
+                out_file.write(content)
+        except FileNotFoundError:
+            print('warning file not found, nothing copied')
 
     def get_aligned_filename(self):
         """
@@ -113,6 +120,18 @@ class SequenceAligner:
         """
         return SequenceAligner.aligned_pattern.format(tag=self.tag,
                                                       file_id=self.already_aligned_file_id)
+
+    @staticmethod
+    def get_filename_by_tag(tag):
+        """
+        DESCRIPTION:
+        Returns the name of the file with the most recently aligned sequences of that tag
+        :return: [string] the filename or None if no such file exists
+        """
+        file_id, x = SequenceAligner.get_actual(tag)
+        if file_id is None:
+            return None
+        return SequenceAligner.aligned_pattern.format(selection_specifier=tag, file_id=file_id)	
 
     @staticmethod
     def get_actual(tag):
@@ -195,9 +214,8 @@ class SequenceAligner:
         stdout, stderr = mafft_cline()
         print('Alignment completed')
         # Write result into file
-        file = open(destname, 'w')
-        file.write(stdout)
-        file.close()
+        with open(destname, 'w') as file:
+            file.write(stdout)
         print('Alignment saved')
 
     def _align_from_existing(self):
--- src/main.py
+++ src/main.py
@@ -20,13 +20,46 @@ def main():
     print('number of records retrieved: ' + str(len(records)))
     timestamp = datetime.fromtimestamp(int(result['request_timestamp'])).strftime("%Y%m%d%H%M%S")
 
-    # update the complete alignment
-    my_aligner = SequenceAligner.from_tag('complete', timestamp)
+    # update the alignment of the N genome
+    n_aligner = SequenceAligner.from_tag(tag='gene_N', data=result)
+    filter1 = Filter(['nucleocapsid phosphoprotein', 'complete cds']).all_filter
+    filter2 = Filter([]).none_filter
+    n_aligner.add_filter(filter1)
+    n_aligner.add_filter(filter2)
+    print(len(n_aligner.get_filtered_records()))
+    n_aligner.make_alignment()
+    n_aligner.copy_aligned_file_unstamped()
+
+    # update the alignment of the M genome
+    m_aligner = SequenceAligner.from_tag(tag='gene_M', data=result)
+    filter1 = Filter(['membrane glycoprotein', 'complete cds']).all_filter
+    filter2 = Filter([]).none_filter
+    m_aligner.add_filter(filter1)
+    m_aligner.add_filter(filter2)
+    print(len(m_aligner.get_filtered_records()))
+    m_aligner.make_alignment()
+    m_aligner.copy_aligned_file_unstamped()
+
+    # update the alignment of the M genome
+    s_aligner = SequenceAligner.from_tag(tag='gene_S', data=result)
+    filter1 = Filter(['surface glycoprotein', 'complete cds']).all_filter
+    filter2 = Filter([]).none_filter
+    s_aligner.add_filter(filter1)
+    s_aligner.add_filter(filter2)
+    print(len(s_aligner.get_filtered_records()))
+    s_aligner.make_alignment()
+    s_aligner.copy_aligned_file_unstamped()
+
+    # update the alignment of the complete genome
+    my_aligner = SequenceAligner.from_tag(tag='complete', data=result)
     complete_filter = Filter(['complete genome']).all_filter
-    my_aligner.set_records(records)
     my_aligner.add_filter(complete_filter)
     my_aligner.make_alignment()
+    my_aligner.copy_aligned_file_unstamped()
 
+
+
+'''
     # iqtree visualizing tree
     print('Select the best alignments')
     origname = my_aligner.get_aligned_filename()
@@ -48,7 +81,7 @@ def main():
     print('Generating tree visualization')
     tree_route = config.TREE_DIR / destname.split('.')[0]
     ete.tree_viewer(tree_route)
-
+'''
 
 if __name__ == '__main__':
     main()
